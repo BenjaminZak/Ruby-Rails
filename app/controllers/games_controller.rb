3471e98e3d1fb1b7243b51d8a9af2063442763c0
BenjaminZak <benjaminzak21@gmail.com>
2016-11-19 04:59:41 +0100

    CRUD possible

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,19 @@ class GamesController < ApplicationController
   def index
   	@games = Game.all
   end
+  def create
+  	Game.create title: params[:title]
+  	redirect_to '/games'
+  end
+  def show
+  	@game = Game.find(params[:id])
+  end
+  def update
+    Game.find(params[:id]).update title: params[:title]
+    redirect_to "/games/#{params[:id]}"
+  end
+  def destroy
+    Game.find(params[:id]).destroy
+    redirect_to "/games"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 root 'pages#home'
 get 'home' => 'pages#home'
-get 'games' => 'games#index'
 
+
+get 'games' => 'games#index'
+get 'games/:id' => 'games#show'
+post 'games' => 'games#create'
+patch 'games/:id' => 'games#update'
+delete 'games/:id' => 'games#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
